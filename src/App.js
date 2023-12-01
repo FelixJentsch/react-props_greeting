55d2f52aa0e4a21e2e7376f7e243bc107a9bcf5b
FelixJentsch <FelixJentsch91@googlemail.com>
2023-12-01 15:42:10 +0100

    challenge solved

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <div>
+      <Greeting name="John Doe" />
+      <Greeting name="Jane Doe" />
+      <Greeting name="Jessica" />
+    </div>
+  );
+}
+
+function Greeting({ name }) {
+  if (name === "Jessica") {
+    return <h1>Hello Coach {name}!</h1>;
+  } else {
+    return <h1>Hello {name}!</h1>;
+  }
 }
